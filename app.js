1111890803230553b5e2153df0dfa87e427c1be4
sselvam02 <ShyamSelvam1034@Gmail.com>
2025-04-20 23:30:10 -0400

    Co-authored-by: xsode <xsoder@users.noreply.github.com>

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express()
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true })); 
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html'); 
+});
+
+app.get('/index', (req, res) => {
+    res.sendFile(__dirname + '/index.html'); 
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(__dirname + '/contact.html'); 
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/about.html'); 
+});
+app.post('/submit-form', (req, res) => {
+    const username = req.body.username; 
+    res.send(`Username is $sselvam`);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
